d5a9167e6347c98a6d3e3e91c537ab71be07bf9e
Paweł Mołek <pawelmolek90@gmail.com>
2016-04-26 21:06:59 +0200

    challage - add ratings by readers

--- IaesteWorkshop/Controllers/HomeController.cs
+++ IaesteWorkshop/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using IaesteWorkshop.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
 
 namespace IaesteWorkshop.Controllers
 {
     public class HomeController : Controller
     {
+        private DBContext db = new DBContext();
+
         public ActionResult Index()
         {
             ViewBag.MenuItem = "index";
@@ -22,25 +28,57 @@ namespace IaesteWorkshop.Controllers
             return View();
         }
 
-        public ActionResult Reviews()
+        public async Task<ActionResult> Reviews(int? page = null)
         {
             ViewBag.MenuItem = "reviews";
+            int pageNumber = Math.Max(1, page ?? 1);
+            int pageSize = 6;
 
-            return View();
+            var moviesQ = db.Movies.Include(x => x.CoverImage)
+                .Include(x => x.Reviews)
+                .Where(x => x.Reviews.Any())
+                .OrderByDescending(x => x.Id);
+
+            ViewBag.TotalPages = (await moviesQ.CountAsync() + pageSize - 1) / pageSize;
+            ViewBag.CurrentPage = pageNumber;
+            var model = await moviesQ
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return View(model);
         }
 
-        public ActionResult Single()
+        public async Task<ActionResult> Single(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             ViewBag.MenuItem = "single";
-
-            return View();
+            var movie = await db.Movies.Include(s => s.CoverImage).FirstOrDefaultAsync(s => s.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
         }
 
-        public ActionResult Videos()
+
+        public async Task<ActionResult> Videos(int? page = null)
         {
             ViewBag.MenuItem = "videos";
+            int pageNumber = Math.Max(1, page ?? 1);
+            int pageSize = 12;
 
-            return View();
+            var moviesQ = db.Movies.Include(x => x.CoverImage).OrderByDescending(x => x.Id);
+
+            ViewBag.TotalPages = (await moviesQ.CountAsync() + pageSize - 1) / pageSize;
+            ViewBag.CurrentPage = pageNumber;
+            var model = await moviesQ
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return View(model);
         }
 
         [ActionName("404")]
@@ -49,6 +87,17 @@ namespace IaesteWorkshop.Controllers
             ViewBag.MenuItem = "404";
 
             return View("Error404");
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
