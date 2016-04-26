38549e9688a24779c12fe7a8dc062f4c0fac10fb
Mateusz Karkula <mateusz.karkula@comarch.com>
2016-04-26 13:29:56 +0200

    Step1

---
+++ IaesteWorkshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IaesteWorkshop.Startup))]
+namespace IaesteWorkshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
